98592c79a363bd8badc35bf8b49de7dd9c0d5c78
jimmy.lee <jimmy.lee@careline.com.tw>
2018-09-15 08:59:13 +0800

    add spec for shareService

--- projectA/src/app/app.component.spec.ts
+++ projectA/src/app/app.component.spec.ts
@@ -11,20 +11,12 @@ describe('AppComponent', () => {
       providers: [ShareService]
     }).compileComponents();
   }));
-  it('should create the app', async(() => {
+
+  it('should return \'test\' ', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    const app = fixture.componentInstance as AppComponent;
+    // It should be wrong
+    expect(app.shareService.getData()).toBe('test');
   }));
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
 });
